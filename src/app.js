19a828f2487dc81b53f9d8b463f3dba25360a70e
TheotimALBERTEAU <149970439+TheotimALBERTEAU@users.noreply.github.com>
2026-03-12 23:36:55 +0100

    ✨ feat:DAO Mongoose v1

---
+++ src/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+if (process.env.DB_MODE === 'mongoose') {
+    require('./dao/mongoose/connection').connect_mongoose();
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
