336e193ffdcaa7aadfa25db20a1cb6beb1e5d636
TheotimALBERTEAU <149970439+TheotimALBERTEAU@users.noreply.github.com>
2026-03-26 22:35:47 +0100

    🚀 up : Update response codes

--- src/services/projects-services.js
+++ src/services/projects-services.js
@@ -13,7 +13,7 @@ module.exports = {
     getCategories: async () => {
         try {
             const Categories = await DAOFactory.getDAOProjects().selectCategories();
-            return {code : "200", message : "Showing all Categories", data : Categories};
+            return {code : 200, message : "Showing all Categories", data : Categories};
         } catch (error) {
             return {code: "521", message : "Error while obtaining categories", data : error};
         }
@@ -22,7 +22,7 @@ module.exports = {
     getByCategory: async (category) => {
         try {
             const ProjectFromCategory = await DAOFactory.getDAOProjects().selectByCategory(category);
-            return {code : "200", message : `Showing projects from category : ${category}`, data : ProjectFromCategory};
+            return {code : 200, message : `Showing projects from category : ${category}`, data : ProjectFromCategory};
         } catch (error) {
             return {code : "522", message : `Error while obtaining projects from category : ${category}`, data : error};
         }
